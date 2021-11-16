54e6da5a81bf3091347b989caf0409731419447a
Paul Laplante <pllplnt2@gmail.com>
2021-11-16 18:20:40 -0500

    Trying to add keyboard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import Keyboard from "react-piano-keyboard";
 
 function App() {
+  const audioContext = new Audio();
+
+  const soundObject = {
+    A: audioContext,
+    B: audioContext,
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Keyboard
+          keyboardWidth={400}
+          classKeyboard={"classKeyboard"}
+          classWhiteKeys={"classWhiteKeys"}
+          classWhiteKey={"classWhiteKey"}
+          classBlackKeys={"classBlackKeys"}
+          classBlackKey={"classBlackKey"}
+          noteSounds={soundObject}
+        />
       </header>
     </div>
   );
